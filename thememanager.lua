e52accf83eb1f6370c2cbe06a986dddfe3143bea
lesserexpense <thundermint78@gmail.com>
2026-02-17 20:03:51 +0200

    Update thememanager.lua

--- thememanager.lua
+++ thememanager.lua
@@ -12,7 +12,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['perk.lol'] 		= { 1, httpService:JSONDecode('{"FontColor":"e6e9ef","MainColor":"1f242d","AccentColor":"3b82f6","BackgroundColor":"181c23","OutlineColor":"323845"}') },
+			['perk.lol'] 		= { 1, httpService:JSONDecode('{"FontColor":"ece5ff","MainColor":"221b2e","AccentColor":"a772ff","BackgroundColor":"191423","OutlineColor":"483a60"}') },
 		['Default'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"0055ff","BackgroundColor":"141414","OutlineColor":"323232"}') },
 		['BBot'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
